3b2c6d7b79d6d93dee3956fc0cfd9d470f450bfb
Pablo Alves <pabloalves.dev@gmail.com>
2023-08-14 13:13:58 -0300

    dddddddddd

--- src/Components/Watch/index.tsx
+++ src/Components/Watch/index.tsx
@@ -10,8 +10,8 @@ interface Props {
 const Watch = ({ t }: Props) => {
     const [tasks, setTasks] = useState<ITask[]>(t);
     const [actualTask, setActualTask] = useState(0);
-    const [leftSeconds, setLeftSeconds] = useState(tasks[actualTask].duration);
-    const [taskTransition, setTaskTransition] = useState(5);
+    const [leftSeconds, setLeftSeconds] = useState(tasks[actualTask].duration || 0);
+    const [allFinished, setAllFinished] = useState<boolean>(false);
     const convertToClock = (sec: number): string => {
         const hour = Math.trunc(sec/3600).toString().length > 1 ? Math.trunc(sec/3600).toFixed() : `0${Math.trunc(sec/3600).toFixed()}`;
         const minutes = Math.trunc(sec/60).toString().length > 1 ? Math.trunc(sec/60).toFixed() : `0${Math.trunc(sec/60).toFixed()}`;;
@@ -23,26 +23,20 @@ const Watch = ({ t }: Props) => {
     }
 
     const startTask = () => {
-        atualizarEstadoTask(actualTask, tasks[actualTask], 'execute')
+        atualizarEstadoTask(actualTask, 'execute')
     }
     const loadNextTask = () => {
         const newTask = actualTask + 1;
-        console.log(`Carregando próxima task: ${tasks[newTask]}`);
-        setActualTask(newTask);
-        setLeftSeconds(tasks[newTask].duration);
+        if(tasks[newTask] !== undefined){
+            console.log(`Carregando próxima task: ${tasks[newTask]}`);
+            setActualTask(newTask);
+            setLeftSeconds(tasks[newTask].duration);
+        } else {
+            setAllFinished(true)
+        }
     }
-
-    // const verifyMode = () => {
-    //     let result;
-    //     if(actualTask.started) result = 'executando';
-    //     //leftSeconds zerado, transicao maior que 0
-    //     if(taskTransition > 0 && leftSeconds === 0) result = 'em transicao';
-    //     //transicao zerada, leftSeconds igual duration, nao pode ter sido iniciado, nao pode ter sido terminado
-    //     if(leftSeconds === actualTask.duration && !actualTask.started && taskTransition === 0 && !actualTask.finished) result = 'aguardando'; 
-    //     console.log(result);
-    //     return result
-    // }
-    const atualizarEstadoTask = (indice: number, novaTask: ITask, action: "waiting" | "execute" | "finished" | "transition") => {
+    
+    const atualizarEstadoTask = (indice: number, action: "waiting" | "execute" | "finished" | "transition") => {
         const novasTasks = [...tasks]; // Criando uma cópia do array de tasks
         novasTasks[indice] = {
             duration: tasks[indice].duration,
@@ -55,13 +49,9 @@ const Watch = ({ t }: Props) => {
     useEffect(()=>{
         if(tasks[actualTask].state === 'execute' && leftSeconds >= 1) countDown();
         if(tasks[actualTask].state === 'execute' && leftSeconds === 0) {
-            atualizarEstadoTask(actualTask, tasks[actualTask], 'finished')
-            setActualTask(a => a+1);
-            loadNextTask();
+            atualizarEstadoTask(actualTask, 'finished')
+            setTimeout(()=>loadNextTask(), 5000)
         }
-        // if(tasks[actualTask].state === 'finished'){
-        //     loadNextTask();
-        // }
         console.log(`Task atual é: ${tasks[actualTask].title} e está ${tasks[actualTask].state}`);
     },[tasks, leftSeconds])
 
@@ -95,17 +85,13 @@ const Watch = ({ t }: Props) => {
                         <Text>{tasks[actualTask].title}</Text>
                     </>
                 )}
-                {tasks[actualTask].state === 'finished' && (
+                {(leftSeconds === 0 && tasks[actualTask].state === "finished" && !allFinished) && (
                     <>
-                        <Text>{convertToClock(taskTransition)}</Text>
-                        <Text>Preparando para a prox TASK</Text>
+                        <Text>Fim da Tarefa {tasks[actualTask].title}</Text>
+                        <Text>Preparando para {tasks[actualTask+1]?.title}</Text>
                     </>
                 )}
-                {/* {(leftSeconds === 0 && taskTransition === 0) && (
-                    <>
-                        <Text>Fim da Tarefa</Text>
-                    </>
-                )} */}
+                {allFinished && <Text>Fim de Tudo, insira novas tasks</Text>}
             </CircularProgressbarWithChildren>;
         </Container>
     )
