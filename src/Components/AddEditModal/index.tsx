d42f0324152c96837ea41920ab28a689c3d2f8d1
Pablo Alves <pabloalves.dev@gmail.com>
2023-08-21 22:55:12 -0300

    ddddd

--- src/Components/AddEditModal/index.tsx
+++ src/Components/AddEditModal/index.tsx
@@ -11,17 +11,31 @@ interface Props {
     item?: ITask;
 }
 
+interface ITime {
+    hour: number;
+    minute: number;
+    second: number;
+}
+
 const AddEditModal = ({ visible, setVisible, item }:Props) => {
     const { tasks, setTasks } = useAppContext();
     const [task, setTask] = useState<ITask>({} as ITask);
+    const [time, setTime] = useState<ITime>({
+        hour: 0,
+        minute: 0,
+        second: 0
+    });
     const [invalid, setInvalid] = useState<boolean>(false);
     const handleClose = () => {
         setVisible(false);
         setTask({} as ITask);
     }
     const handleSave = () => {
-        setTasks(prev => ([...prev, task]));
-        handleClose();
+        const totalTime = time.hour*3600+time.minute*60+time.second;
+        setTask(prev => ({...prev, duration: totalTime}))
+        alert(JSON.stringify(task));
+        // setTasks(prev => ([...prev, task]));
+        // handleClose();
     }
     const valid = (e: React.ChangeEvent<HTMLInputElement>) => {  
         if(tasks.map(a => a.title).includes(e.target.value)) setInvalid(true)
@@ -43,11 +57,11 @@ const AddEditModal = ({ visible, setVisible, item }:Props) => {
                 </Row>
                 <Row>
                     <Col>
-                        <InputLabel>Duração</InputLabel>
+                        <InputLabel style={{width: '100%'}}>Duração</InputLabel>
                         <Row>
-                            <TimeInput maxLength={2} value={task.duration} onChange={(a: any) => setTask(prev => ({...prev, duration: a.target.value}))} type="text" />
-                            <TimeInput maxLength={2} value={task.duration} onChange={(a: any) => setTask(prev => ({...prev, duration: a.target.value}))} type="text" />
-                            <TimeInput maxLength={2} value={task.duration} onChange={(a: any) => setTask(prev => ({...prev, duration: a.target.value}))} type="text" />
+                            <TimeInput maxLength={2} value={time.hour} onChange={(a: any) => setTime(prev => ({...prev, hour: a.target.value}))} type="text" />
+                            <TimeInput maxLength={2} value={time.minute} onChange={(a: any) => setTime(prev => ({...prev, minute: a.target.value}))} type="text" />
+                            <TimeInput maxLength={2} value={time.second} onChange={(a: any) => setTime(prev => ({...prev, second: a.target.value}))} type="text" />
                         </Row>
                     </Col>
                 </Row>
